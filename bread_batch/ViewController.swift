9ff118b5086dc5365230b9f5019607123fe00ea1
David Uzumaki <iamdaviduzumaki@gmail.com>
2020-03-29 05:03:11 +0100

    Ability to add image

--- bread_batch/ViewController.swift
+++ bread_batch/ViewController.swift
@@ -8,13 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UINavigationControllerDelegate,UIImagePickerControllerDelegate {
+    @IBOutlet var mainImageArea: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
 
+    @IBAction func addImageButton(_ sender: UIButton) {
+           imagePicker.allowsEditing = false
+           imagePicker.sourceType = .photoLibrary
+           
+           present(imagePicker, animated: true, completion: nil)
+       }
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
     }
-
-
+  
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            mainImageArea.contentMode = .scaleAspectFit
+            mainImageArea.image       = pickedImage
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
 
