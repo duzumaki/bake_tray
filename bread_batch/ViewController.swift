15b54c180196f4f06445dc0f8256257628f3489d
David Uzumaki <iamdaviduzumaki@gmail.com>
2020-05-17 22:51:07 +0100

    long press menu items added

--- bread_batch/ViewController.swift
+++ bread_batch/ViewController.swift
@@ -9,38 +9,72 @@
 import UIKit
 import SwiftUI
 
-class ViewController: UIViewController,UINavigationControllerDelegate,UIImagePickerControllerDelegate {
+class ViewController: UIViewController,UINavigationControllerDelegate,UIImagePickerControllerDelegate, UITextFieldDelegate {
     @IBOutlet weak var mainImageArea: UIImageView!
     @IBOutlet weak var contentImageArea: UIImageView!
-    
-    
-    
     let imagePicker = UIImagePickerController()
-    
-    
+ 
     
     @IBAction func addImageButton(_ sender: UIButton) {
        imagePicker.allowsEditing = false
        imagePicker.sourceType = .photoLibrary
 
-       contentImageArea.layer.zPosition = 0
-       mainImageArea.layer.zPosition = 1
-       
        present(imagePicker, animated: true, completion: nil)
     }
+    
   
     override func viewDidLoad() {
         super.viewDidLoad()
         mainImageArea.image = UIImage(named: "main2000.png")
         
+        contentImageArea.layer.zPosition = 0
+        mainImageArea.layer.zPosition = 1
+        
+        
+        contentImageArea.isUserInteractionEnabled = true
+        let contentImageAreaLongPress = UILongPressGestureRecognizer(target: self, action: #selector(self.replaceEditDeleteLongPressContentImage))
+        
+        contentImageArea.addGestureRecognizer(contentImageAreaLongPress)
+        
+        
         imagePicker.delegate = self
+        
+    }
+    
+    override var canBecomeFirstResponder: Bool{
+        return true
+    }
+    
+    @objc func replaceEditDeleteLongPressContentImage(sender: UILongPressGestureRecognizer){
+        if sender.state == .began {
+            let menu = UIMenuController.shared
+            becomeFirstResponder()
+            
+            let menuItemCrop = UIMenuItem(title: "crop", action: #selector(handleMenuItemAction))
+            let menuItemReplace = UIMenuItem(title: "replace", action: #selector(handleMenuItemAction))
+            let menuItemDelete = UIMenuItem(title: "delete", action: #selector(handleMenuItemAction))
+            menu.menuItems = [menuItemCrop,menuItemReplace,menuItemDelete]
+            
+            let location = sender.location(in: sender.view)
+            let menuLocation = CGRect(x: location.x, y:location.y, width: 0, height: 0)
+            menu.showMenu(from: sender.view!, rect: menuLocation)
+            
+        }
+        
+    }
+    
+    @objc func handleMenuItemAction(){
+        print("Menu Item tapped")
+        
     }
   
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             contentImageArea.contentMode = .scaleToFill
             contentImageArea.image       = pickedImage
         }
+        
         
         dismiss(animated: true, completion: nil)
     }
